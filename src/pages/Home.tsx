58f6570d80f6ab5042c2997d41b82aa258e598bc
Markus Kitunen <markus.kitunen@outlook.com>
2023-12-15 15:34:06 +0200

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCard, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { useState } from 'react';
+
+class Unit {
+  id: string
+  name: string
+  moistLimit: number
+  waterTime: number
+  moistMeasureIntervall: number
+  constructor(id: string, name: string, moistLimit: number, waterTime: number, moistMeasureIntervall: number) {
+    this.id = id;
+    this.name = name
+    this.moistLimit = moistLimit
+    this.waterTime = waterTime
+    this.moistMeasureIntervall = moistMeasureIntervall
+  }
+}
 
 const Home: React.FC = () => {
+
+  const [units, setUnits] = useState([
+    {uni: "", name: "" }
+  ])
+
+  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle slot='secondary'>My plants</IonTitle>
+          <IonButtons slot='end'>
+            <IonButton color={"primary"}>REFRESH</IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <IonCard>
+          
+        </IonCard>
       </IonContent>
     </IonPage>
   );
