575ca83e946939bee404d4582078cf9be4f32f1b
Markus Kitunen <markus.kitunen@outlook.com>
2023-12-20 12:14:14 +0100

    refresh function added, settings buttton justified to end

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { IonAlert, IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonProgressBar, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import { settingsOutline } from 'ionicons/icons'
 import './Home.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IUnitState } from '../interfaces';
 import Log from '../components/Log';
 import Settings from '../components/Settings';
@@ -14,6 +14,12 @@ const Home: React.FC = () => {
   const [unit3, setUnit3] = useState<IUnitState>({ id: "Unit 3", name: "Plant name", status: "OK", moistLevel: 0.3, moistLimit: 15000, waterTime: 20, moistMeasureIntervall: 7, logs: ["12.12.2023 12:00", "19.12.2023 12:00", "28.12.2023 12:00", "29.12.2023 12:00"] })
   const [unit4, setUnit4] = useState<IUnitState>({ id: "Unit 4", name: "Plant name", status: "OK", moistLevel: 0.9, moistLimit: 15000, waterTime: 25, moistMeasureIntervall: 5, logs: ["12.12.2023 12:00", "16.12.2023 12:00", "20.12.2023 12:00", "25.12.2023 12:00"] })
   const units = [{unit: unit1, setUnit: setUnit1}, {unit: unit2, setUnit: setUnit2}, {unit: unit3, setUnit: setUnit3}, {unit: unit4, setUnit: setUnit4}]
+
+
+  const refresh = () => {
+    /* fetch data */
+
+  }
 
   const setColor = (unit: IUnitState) => {
     if (unit.moistLevel < 0.33) {
@@ -43,7 +49,7 @@ const Home: React.FC = () => {
         <IonToolbar>
           <IonTitle slot='secondary'>My plants</IonTitle>
           <IonButtons slot='end'>
-            <IonButton color={"primary"}>REFRESH</IonButton>
+            <IonButton color={"primary"} onClick={refresh}>REFRESH</IonButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
@@ -54,8 +60,8 @@ const Home: React.FC = () => {
               <IonRow>
                 <IonCol size='2'><p>{u.unit.id}</p></IonCol>
                 <IonCol size='8'><h2 className='align-center'>{u.unit.name}</h2></IonCol>
-                <IonCol size='2'>
-                  <IonButtons>
+                <IonCol size='2' class='ion-padding-start'>
+                  <IonButtons class='ion-justify-content-end'>
                     <IonButton id={`${u.unit.id}-settings`}>
                       <IonIcon icon={settingsOutline}></IonIcon>
                     </IonButton>
@@ -71,20 +77,14 @@ const Home: React.FC = () => {
                 <IonCol><p className='align-center'>Status: <IonText color='danger'>{u.unit.status}</IonText></p></IonCol>
                 }
               </IonRow>
-              <IonRow>
-                <IonCol size='6' offset='6'>
+              <IonRow class="ion-justify-content-center">
+                <IonCol class="ion-align-self-end" size='auto'>
                   <IonText>
-                    <p className='moist-percent'>{u.unit.moistLevel * 100}%</p>
-                  </IonText>
-                </IonCol>
-              </IonRow>
-              <IonRow class="ion-align-items-center">
-                <IonCol size='5'>
-                  <IonText>
-                    <p className='align-center'>Moist level:</p>
+                    <p style={{textAlign: "end" }}>Moist level:</p>
                   </IonText>
                 </IonCol>
                 <IonCol size='6'>
+                  <p className='moist-percent'>{u.unit.moistLevel * 100}%</p>
                   <IonProgressBar value={u.unit.moistLevel} color={setColor(u.unit)}></IonProgressBar>               
                 </IonCol>
               </IonRow>
