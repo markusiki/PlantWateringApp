6f7708060ee1c78c6fe5d370eca05bee1eb2c828
Markus <markus.kitunen@outlook.com>
2024-02-17 13:33:57 +0200

    waterPlant added

--- src/services/units.ts
+++ src/services/units.ts
@@ -1,9 +1,41 @@
 import axios from "axios";
-const baseUrl = '/api/plants'
+import { ISettingsProps } from "../interfaces";
+const baseUrl = '/api/units'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const getAll = async () => {
+  try {
+    const request = axios.get(baseUrl)
+    const response = await request
+    return response
+  }
+  catch (error) {
+    console.log(error)
+  }
 }
 
-export default { getAll }
+const changeSettigs = async (newUnits: ISettingsProps, id: string) => {
+  try {
+    const request = axios.put(`${baseUrl}/${id}`, newUnits)
+    const response = await request
+    return response.data
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+const waterPlant = async (id: string) => {
+  try {
+    const request = axios.post(`${baseUrl}/${id}`)
+    const response = await request
+    return response.status
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+
+const unitServices = { getAll, changeSettigs, waterPlant }
+
+export default unitServices
