e0d72455b9c56b2589c4e5add6a6e0d6b390a430
Markus <markus.kitunen@outlook.com>
2024-03-27 19:39:55 +0200

    created

---
+++ backend/createUser.py
@@ -0,0 +1,34 @@
+# Create user for the app and saves the created user to the users.json database.
+# If database does not exist, it will be creted and initialised
+
+from flask_bcrypt import Bcrypt
+import json
+
+db = "users.json"
+bcrypt = Bcrypt()
+
+
+while True:
+    try:
+        file = open(db, "r+")
+        users = json.load(file)
+        username = input("Enter username: ")
+        for user in users:
+            if user["username"] == username:
+                print("Username already exists.")
+                break
+        else:
+            password = input("Enter password: ")
+            hash = bcrypt.generate_password_hash("password", 10).decode("utf-8")
+            print(hash)
+            user = {"username": username, "passwordHash": hash}
+            users.append(user)
+            file.seek(0)
+            json.dump(users, file, indent=3)
+            file.close()
+            break
+
+    except FileNotFoundError:
+        file = open(db, "w")
+        json.dump([], file)
+        file.close()
