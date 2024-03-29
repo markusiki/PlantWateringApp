64ccca9e83e177e56da6b49809d5433f20274b77
Markus <markus.kitunen@outlook.com>
2024-03-29 15:18:40 +0200

    style added

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Login.css'
 import {
   IonButton,
   IonButtons,
@@ -9,6 +10,7 @@ import {
   IonInput,
   IonItem,
   IonList,
+  IonPage,
 } from '@ionic/react'
 import { ILoginProps } from '../interfaces'
 
@@ -20,38 +22,38 @@ const Login: React.FC<ILoginProps> = ({
   handleLogin,
 }) => {
   return (
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>Login</IonCardTitle>
-      </IonCardHeader>
-      <IonCardContent>
-        <IonList>
-          <IonItem>
-            <IonInput
-              label="Username"
-              name="username"
-              value={username}
-              type="text"
-              maxlength={40}
-              onInput={(event) => setUsername((event.target as HTMLInputElement).value)}
-            ></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonInput
-              label="Password"
-              name="password"
-              value={password}
-              type="password"
-              maxlength={50}
-              onInput={(event) => setPassword((event.target as HTMLInputElement).value)}
-            ></IonInput>
-          </IonItem>
-        </IonList>
-        <IonButtons slot="secondary">
+    <IonPage>
+      <IonCard className="login">
+        <IonCardHeader>
+          <IonCardTitle>Login</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <IonList>
+            <IonItem>
+              <IonInput
+                label="Username"
+                name="username"
+                value={username}
+                type="text"
+                maxlength={40}
+                onInput={(event) => setUsername((event.target as HTMLInputElement).value)}
+              ></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonInput
+                label="Password"
+                name="password"
+                value={password}
+                type="password"
+                maxlength={50}
+                onInput={(event) => setPassword((event.target as HTMLInputElement).value)}
+              ></IonInput>
+            </IonItem>
+          </IonList>
           <IonButton onClick={(event) => handleLogin(event)}>Login</IonButton>
-        </IonButtons>
-      </IonCardContent>
-    </IonCard>
+        </IonCardContent>
+      </IonCard>
+    </IonPage>
   )
 }
 export default Login
