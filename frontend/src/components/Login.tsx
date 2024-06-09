79d13cf3692bb9feed99f66a2a20e9311d47e897
Markus <markus.kitunen@outlook.com>
2024-06-09 16:31:36 +0300

    Login page responsiveness improved

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -6,10 +6,13 @@ import {
   IonCardContent,
   IonCardHeader,
   IonCardTitle,
+  IonCol,
+  IonGrid,
   IonInput,
   IonItem,
   IonList,
   IonPage,
+  IonRow,
 } from '@ionic/react'
 import { ILoginProps } from '../interfaces'
 
@@ -21,38 +24,44 @@ const Login: React.FC<ILoginProps> = ({
   handleLogin,
 }) => {
   return (
-    <IonPage>
-      <IonCard className="login">
-        <IonCardHeader>
-          <IonCardTitle>Login</IonCardTitle>
-        </IonCardHeader>
-        <IonCardContent>
-          <IonList>
-            <IonItem>
-              <IonInput
-                label="Username"
-                name="username"
-                value={username}
-                type="text"
-                maxlength={40}
-                onInput={(event) => setUsername((event.target as HTMLInputElement).value)}
-              ></IonInput>
-            </IonItem>
-            <IonItem>
-              <IonInput
-                label="Password"
-                name="password"
-                value={password}
-                type="password"
-                maxlength={50}
-                onInput={(event) => setPassword((event.target as HTMLInputElement).value)}
-              ></IonInput>
-            </IonItem>
-          </IonList>
-          <IonButton onClick={(event) => handleLogin(event)}>Login</IonButton>
-        </IonCardContent>
-      </IonCard>
-    </IonPage>
+    <>
+      <IonGrid fixed={true}>
+        <IonRow class="ion-align-items-center ion-justify-content-center">
+          <IonCol sizeXs="12" sizeLg="7" sizeXl="7">
+            <IonCard className="login">
+              <IonCardHeader>
+                <IonCardTitle>Login</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <IonList>
+                  <IonItem>
+                    <IonInput
+                      label="Username"
+                      name="username"
+                      value={username}
+                      type="text"
+                      maxlength={40}
+                      onInput={(event) => setUsername((event.target as HTMLInputElement).value)}
+                    ></IonInput>
+                  </IonItem>
+                  <IonItem>
+                    <IonInput
+                      label="Password"
+                      name="password"
+                      value={password}
+                      type="password"
+                      maxlength={50}
+                      onInput={(event) => setPassword((event.target as HTMLInputElement).value)}
+                    ></IonInput>
+                  </IonItem>
+                </IonList>
+                <IonButton onClick={(event) => handleLogin(event)}>Login</IonButton>
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </>
   )
 }
 export default Login
