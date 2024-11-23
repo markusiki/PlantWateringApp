3bcebc1fac144f5df1c4fa6dba0c460d82b8e016
Markus <markus.kitunen@outlook.com>
2024-11-23 18:36:05 +0200

    unit schema updated

--- backend/plant_api/schemas.py
+++ backend/plant_api/schemas.py
@@ -4,7 +4,7 @@ from marshmallow import Schema, fields, validate
 class UnitsSchema(Schema):
     id = fields.Str(required=True, validate=validate.Length(equal=5))
     name = fields.Str(requiered=True, validate=validate.Length(min=2, max=100))
-    moistLimit = fields.Int(required=True, validate=validate.Range(min=10000, max=18000))
+    moistLimit = fields.Int(required=True, validate=validate.Range(min=0, max=100))
     waterTime = fields.Int(required=True, validate=validate.Range(min=0, max=180))
     enableAutoWatering = fields.Bool(required=True)
     enableMaxWaterInterval = fields.Bool(required=True)
