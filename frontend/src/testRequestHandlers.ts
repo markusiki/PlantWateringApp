bc37c78bd5bb0669da9dbacacb3f5ec4bf3a43d6
Markus Kitunen <markus.kitunen@outlook.com>
2025-01-30 17:53:21 +0200

    console.logs removed

--- frontend/src/testRequestHandlers.ts
+++ frontend/src/testRequestHandlers.ts
@@ -3,29 +3,28 @@ import { setupServer } from 'msw/node'
 import { http, HttpResponse, PathParams } from 'msw'
 import { IDeviceSettingsState, IUnitState } from './interfaces'
 
-const testUnits: IUnitState[] = 
-[
+const testUnits: IUnitState[] = [
   {
-  id: 'Unit1',
-  name: 'Test1',
-  status: 'OK',
-  moistValue: 12000,
-  moistLimit: 14000,
-  waterTime: 5,
-  enableAutoWatering: false,
-  enableMaxWaterInterval: false,
-  enableMinWaterInterval: false,
-  maxWaterInterval: 0,
-  minWaterInterval: 0,
-  logs: [
-    {
-      date: '27.08.2024 10:54',
-      status: 'OK',
-      moistValue: 12000,
-      watered: true,
-      waterMethod: 'auto: max watering interval',
-    },
-  ],
+    id: 'Unit1',
+    name: 'Test1',
+    status: 'OK',
+    moistValue: 12000,
+    moistLimit: 14000,
+    waterTime: 5,
+    enableAutoWatering: false,
+    enableMaxWaterInterval: false,
+    enableMinWaterInterval: false,
+    maxWaterInterval: 0,
+    minWaterInterval: 0,
+    logs: [
+      {
+        date: '27.08.2024 10:54',
+        status: 'OK',
+        moistValue: 12000,
+        watered: true,
+        waterMethod: 'auto: max watering interval',
+      },
+    ],
   },
   {
     id: 'Unit2',
@@ -92,37 +91,31 @@ const testUnits: IUnitState[] =
         waterMethod: 'auto: max watering interval',
       },
     ],
-  }
+  },
 ]
 
 let testDeviceSettings: IDeviceSettingsState = {
   runTimeProgram: false,
   moistMeasureInterval: 1,
-  numberOfUnits: 4
-} 
-
- 
+  numberOfUnits: 4,
+}
 
 export const restHandlers = [
   http.post('/api/login', () => {
     return HttpResponse.json()
   }),
   http.get('/api/units', async () => {
-    console.log(testUnits.slice(0, testDeviceSettings.numberOfUnits))
-    console.log('test device settings', testDeviceSettings)
     return HttpResponse.json(testUnits.slice(0, testDeviceSettings.numberOfUnits))
   }),
   http.get('/api/device', async () => {
     return HttpResponse.json(testDeviceSettings)
   }),
-  http.put<PathParams, IDeviceSettingsState, IDeviceSettingsState>('/api/device', async ({request}) => {
+  http.put<PathParams, IDeviceSettingsState, IDeviceSettingsState>('/api/device', async ({ request }) => {
     const updatedSettings = await request.json()
     testDeviceSettings = updatedSettings
     return HttpResponse.json(testDeviceSettings)
-
-  })
+  }),
 ]
-
 
 const server = setupServer(...restHandlers)
 
