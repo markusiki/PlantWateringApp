547f7704506bd8d76ac5929ebb8a8a6526a9f28c
Markus Kitunen <markus.kitunen@outlook.com>
2025-03-11 18:22:05 +0200

    added attributes

--- backend/plant_api/schemas.py
+++ backend/plant_api/schemas.py
@@ -2,18 +2,20 @@ from marshmallow import Schema, fields, validate
 
 
 class UnitsSchema(Schema):
-    id = fields.Str(required=True, validate=validate.Length(equal=5))
-    name = fields.Str(requiered=True, validate=validate.Length(min=2, max=100))
-    moistLimit = fields.Int(required=True, validate=validate.Range(min=0, max=100))
-    waterTime = fields.Int(required=True, validate=validate.Range(min=0, max=180))
-    enableAutoWatering = fields.Bool(required=True)
-    enableMaxWaterInterval = fields.Bool(required=True)
-    enableMinWaterInterval = fields.Bool(required=True)
-    maxWaterInterval = fields.Int(required=True, validate=validate.Range(min=1, max=180))
-    minWaterInterval = fields.Int(required=True, validate=validate.Range(min=1, max=180))
+    id = fields.Str(metadata={'required': True}, validate=validate.Length(equal=5))
+    name = fields.Str(metadata={'required': True}, validate=validate.Length(min=2, max=100))
+    moistLimit = fields.Int(metadata={'required': True}, validate=validate.Range(min=0, max=100))
+    waterTime = fields.Int(metadata={'required': True}, validate=validate.Range(min=0, max=180))
+    enableAutoWatering = fields.Bool(metadata={'required': True})
+    enableMaxWaterInterval = fields.Bool(metadata={'required': True})
+    enableMinWaterInterval = fields.Bool(metadata={'required': True})
+    maxWaterInterval = fields.Int(metadata={'required': True}, validate=validate.Range(min=1, max=180))
+    minWaterInterval = fields.Int(metadata={'required': True}, validate=validate.Range(min=1, max=180))
+    waterFlowRate = fields.Decimal(metadata={'required': True})
 
 
 class DeviceSchema(Schema):
-    runTimeProgram = fields.Bool(required=True)
-    moistMeasureInterval = fields.Int(required=True, validate=validate.Range(min=1, max=100))
-    numberOfUnits = fields.Int(required=True, validate=validate.Range(min=1, max=4))
+    runTimeProgram = fields.Bool(metadata={'required': True})
+    moistMeasureInterval = fields.Int(metadata={'required': True}, validate=validate.Range(min=1, max=100))
+    numberOfUnits = fields.Int(metadata={'required': True}, validate=validate.Range(min=1, max=4))
+    waterAmount = fields.Int(metadata={'required': True}, validate=validate.Range(min=1, max=100000))
