9759223b599e082a220e4d8f2fa329e436c74a59
Markus Kitunen <markus.kitunen@outlook.com>
2025-04-30 15:29:01 +0300

    bugs fixed and updated

--- bff/src/controllers/deviceRegistartionRouter.ts
+++ bff/src/controllers/deviceRegistartionRouter.ts
@@ -1,54 +1,39 @@
 import express from 'express'
 import User from '../models/user'
-import config from '../utils/config'
 import iotService from '../utils/iotService'
-import { IUser } from '../interfaces'
-import axios from 'axios'
+import { IRegisterBody, IUser } from '../interfaces'
 import { HydratedDocument } from 'mongoose'
 
 const deviceRegistrationRouter = express.Router()
 
 deviceRegistrationRouter.post('/', async (req, res) => {
-  const { username, pwhash, serial, rpi_serial } = req.body
+  const { username, pwhash, serial, rpi_serial }: IRegisterBody = req.body
 
   const usernameExists = await User.exists({ username: username })
   if (usernameExists) {
     res.status(400).json({ error: 'Username already exists' })
     return
   }
-  const rpiRegistered = await User.exists({ devices: { serial: serial } })
+  const rpiRegistered = await User.exists({ 'devices.serial': serial })
   if (rpiRegistered) {
     res.status(400).json({ error: 'Device already registered to user' })
     return
   }
 
-  const requestBody = {
-    name: rpi_serial,
-    wormhole_enabled: true,
-  }
-
-  const auth = {
-    headers: {
-      Authorization: `Token ${iotService.authToken}`,
-    },
-  }
-
-  const response = await axios.put(`${config.IOTSERVICE_URI}/devices/${serial}/`, requestBody, auth)
-
-  const data = await response.data
+  const response = await iotService.openWormhole(rpi_serial, serial)
 
   const user: HydratedDocument<IUser> = new User({
     username: username,
     pwhash: pwhash,
     devices: {
       serial: serial,
-      wormhole_slug: data.wormhole_slug,
+      wormhole_slug: response.wormhole_slug,
     },
   })
 
   await user.save()
 
-  res.status(201).send()
+  res.status(201).json({ message: 'Device registered successfully' })
 })
 
 export default deviceRegistrationRouter
