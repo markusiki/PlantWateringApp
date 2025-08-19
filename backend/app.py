fa211ff817c4a60d10ed08f073f0d8aea3db2b82
Markus Kitunen <markus.kitunen@outlook.com>
2025-08-19 14:05:05 +0300

    jwt refresher removed, expired token callback response message changed

--- backend/app.py
+++ backend/app.py
@@ -20,21 +20,6 @@ setTimeProgram()
 
 @jwt.expired_token_loader
 def expired_token_callback(header, data):
-    response = jsonify({"status": 401, "message": "logout successful"})
+    response = jsonify({"status": 401, "message": "Session expired. Please login again!"})
     unset_jwt_cookies(response)
     return response, 401
-
-
-@app.after_request
-def refresh_expiring_jwts(response):
-    try:
-        exp_timestamp = get_jwt()["exp"]
-        now = datetime.now(timezone.utc)
-        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
-        if target_timestamp > exp_timestamp:
-            access_token = create_access_token(identity=get_jwt_identity())
-            set_access_cookies(response, access_token)
-        return response
-    except (RuntimeError, KeyError):
-        # Case where there is not a valid JWT. Just return the original response
-        return response
