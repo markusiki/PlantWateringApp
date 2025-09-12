47c4ebf6eedd546c0cb7c138668020b2c2b33b1e
Markus Kitunen <markus.kitunen@outlook.com>
2025-09-12 10:55:04 +0300

    changed to use units and device models

--- backend/tests/test_helpers/create_db.py
+++ backend/tests/test_helpers/create_db.py
@@ -1,13 +1,12 @@
-import os
 import json
-from .units import units
+from plant_api.databases.models.units import units_model
 from .users import users
-from .device import device
+from plant_api.databases.models.device import device_model
 
 
 def create_test_units_db(path):
     file = open(path, "w")
-    json.dump(units, file)
+    json.dump(units_model, file)
     file.close()
 
 
@@ -19,5 +18,5 @@ def create_test_users_db(path):
 
 def create_test_device_db(path):
     file = open(path, "w")
-    json.dump(device, file)
+    json.dump(device_model, file)
     file.close()
